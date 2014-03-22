1a6f09fe0ed5704c84a34153e09e31c77d24b6bd
Mori <keitamori@gmail.com>
2014-03-22 13:18:08 +0100

    Travail sur la page recherche d'un banc

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Bankpublic::Application.routes.draw do
   get 'bancfinder/find'
 
   root :to => "home#index"
+
+  #Routes de base du site internet
   get '/home/contact' => "home#contact"
   get '/home/informations' => "home#informations"
+
+
+  #Routes pour le finder
+  get '/home/bancfinder' => "bancfinder#find"
+
+  #Routes pour l'authentification utilisateur
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
