b71d562bac4ed298c466a16457969101783ed272
Mori <keitamori@gmail.com>
2014-03-22 11:40:01 +0100

    Ajout de routes pour les pages informations et contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bankpublic::Application.routes.draw do
   root :to => "home#index"
+  get '/home/contact' => "home#contact"
+  get '/home/informations' => "home#informations"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
